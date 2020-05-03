f8ccdb01b7a468af7c7fa8fd81fec2bf2953a222
janardan1995 <44210489+janardan1995@users.noreply.github.com>
2020-05-04 04:43:02 +0530

    [JD]Refactor. Update the code as redish Cache requirements

---
+++ Repository/RedisCacheSevcice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StackExchange.Redis;
+
+namespace Repository
+{
+    public class RedisCacheSevcice
+    {
+        public string RedisConnectionGetValue(string key)
+        {
+            ConnectionMultiplexer RedisCache = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+            IDatabase data = RedisCache.GetDatabase();
+
+            string KeyValue = data.StringGet(key);
+            return KeyValue;
+        }
+
+        public void RedisConnectionSetKeyValue(string key, string value)
+        {
+            ConnectionMultiplexer RadisCache = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+            IDatabase data = RadisCache.GetDatabase();
+
+            data.StringSet(key, value);
+            data.StringGet(key);
+        }
+    }  
+}
